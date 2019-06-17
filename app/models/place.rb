d960d53ebb0497dd8dd702cd2856748e7a4af992
Art <artelo.studio@gmail.com>
2019-06-17 03:30:50 +0000

    delete function added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :delete_all
   has_many :photos
   geocoded_by :address
   after_validation :geocode
+  
+
   
   validates :name, presence: true
 
